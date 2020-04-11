2434e04d2050384b60ba77d4a326033e14cd3c6f
Taylor Sather <fivetentaylor@gmail.com>
2020-04-11 12:11:51 -0700

    Adding tests

--- leet_solutions/taylor_min_distance.py
+++ leet_solutions/taylor_min_distance.py
@@ -1,7 +1,8 @@
-class Solution:
-    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        dist = 0
-        for (x0, y0), (x1, y1) in zip(points, points[1:]):
-            dist += max(abs(x0 - x1), abs(y0 - y1))
+from typing import List
 
-        return dist
+def minTimeToVisitAllPoints(points: List[List[int]]) -> int:
+    dist = 0
+    for (x0, y0), (x1, y1) in zip(points, points[1:]):
+        dist += max(abs(x0 - x1), abs(y0 - y1))
+
+    return dist
---
+++ leet_solutions/test_taylor_min_distance.py
@@ -0,0 +1,5 @@
+from taylor_min_distance import minTimeToVisitAllPoints
+
+def test_min_time():
+    points = [[0,0], [1,5], [10,5]]
+    assert minTimeToVisitAllPoints(points) == 14
